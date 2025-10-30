eace665b174be3eed52fb0e57cae88896142fbad
Yumna <yumnafatani@gmail.com>
2025-10-30 04:39:26 +0300

    Add Instructor class
    Created Instructor class with instructor name property and basic getter/setter methods.

---
+++ Instructor.java
@@ -0,0 +1,17 @@
+public class Instructor {
+    private String iName;
+
+    public Instructor() {}
+
+    public Instructor(String iName) {
+        this.iName = iName;
+    }
+
+    public String getIName() {
+        return iName;
+    }
+
+    public void setIName(String iName) {
+        this.iName = iName;
+    }
+}
